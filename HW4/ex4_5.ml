bb73f82b6f23af7bd85956263417a59efd79f793
ckswjd99 <ckswjd99@snu.ac.kr>
2022-04-18 00:02:33 +0900

    submit, ex4-5 problematic

--- HW4/ex4_5.ml
+++ HW4/ex4_5.ml
@@ -6,11 +6,157 @@ type map =
 | Guide of string * map
 exception IMPOSSIBLE
 
+type rawKey = Bar | Var of int | Node of rawKey * rawKey
+type envi = (map * rawKey) list
+
+let exist_env (env : envi) (expr : map) = (
+  List.exists (fun (_map, _rawKey) -> _map = expr) env
+)
+
+let lookup_env_opt (env : envi) (expr : map) = (
+  let (_, result) = List.find (fun (_map, _rawKey) -> _map = expr) env in
+  result
+)
+
+let rec eval ((env, expr, varnum) : (envi * map * int)) : (envi * int) = 
+  match expr with 
+  | End StarBox -> (
+    if exist_env env expr
+    then (env, varnum)
+    else ((expr, Bar)::env, varnum)
+  )
+  | End (NameBox boxName) -> (
+    if exist_env env expr
+    then (env, varnum)
+    else ((expr, Var (varnum+1))::env, varnum+1)
+  )
+  | Branch (expr1, expr2) -> (
+    let (env', varnum') = eval (env, expr2, varnum) in
+    let key2 = lookup_env_opt env' expr2 in
+    let env'' = (expr, Var (varnum'+1))::env' in
+    let env''' = (expr1, Node (key2, Var (varnum'+1)))::env'' in
+    eval (env''', expr1, varnum'+1)
+  )
+  | Guide (boxName, expr1) -> (
+    let (env', varnum') = eval (env, expr1, varnum) in
+    let (env'', varnum'') = eval (env', (End (NameBox boxName)), varnum') in
+    let key1 = lookup_env_opt env'' expr1 in
+    let key2 = lookup_env_opt env'' (End (NameBox boxName)) in
+    ((expr, Node (key1, key2))::env'', varnum'')
+  )
+
+let rec collect_rules set rules expr = (
+  match rules with
+  | [] -> set
+  | (_map, _rawKey)::ruleTail -> (
+    if _map = expr
+    then collect_rules (_rawKey::set) ruleTail expr
+    else collect_rules set ruleTail expr
+  )
+)
+
+let find_sets rules = List.map (fun (_map, _) -> (_map, (collect_rules [] rules _map))) rules
+
+let find_nested set = List.filter (fun (_map, _rawKeyList) -> List.length _rawKeyList > 1) set
+
+let remove_dupl set = (
+  let rec remove_iter set = 
+    match set with
+    | [] -> set
+    | h::t -> if List.mem h t then remove_iter t else h::(remove_iter t)
+  in
+  remove_iter set
+)
+
+let remove_map set = List.map (fun (_, _rawKey) -> _rawKey) set
+
+let is_box (_map, _) = (
+  match _map with
+  | End _ -> true
+  | _ -> false
+)
+
+let only_namebox_rule rules = List.filter is_box rules
 
 
-let eval mem expr =
-  match expr with
-  | End StarBox -> 
-  | End (NameBox boxName) ->
-  | Branch (expr1, expr2) ->
-  | Guide (boxName, expr1) -> 
+
+(* Let's find expanding paths *)
+let rec traverse _map _name = (
+  match _map with
+  | Node (map1, map2) -> (traverse map1 _name) || (traverse map2 _name)
+  | Var name -> name = _name
+  | Bar -> false
+)
+
+let rec match_key rawKey1 rawKey2 = (
+  match rawKey1, rawKey2 with
+  | (Var name1, Var name2) -> if name1 = name2 then [] else [(rawKey1, rawKey2)]
+  | (Var name1, Node (map21, map22)) -> (
+    if traverse rawKey2 name1 then raise IMPOSSIBLE else [(rawKey1, rawKey2)]
+  )
+  | (Node (map11, map12), Var name2) -> (
+    if traverse rawKey1 name2 then raise IMPOSSIBLE else [(rawKey2, rawKey1)]
+  )
+  | (Node (map11, map12), Node (map21, map22)) -> (match_key map11 map21) @ (match_key map21 map22)
+  | (Var name1, Bar) | (Bar, Var name1) -> [(Var name1, Bar)]
+  | _ -> raise IMPOSSIBLE
+)
+
+let rec resolve_matches nested_list = 
+  match nested_list with
+  | [] -> []
+  | nestedHead::nestedTail -> (
+    let rec resolve_match nested = 
+      match nested with
+      | [] -> []
+      | nHead::nTail -> (
+        List.fold_left (fun alreadyList y -> alreadyList @ (match_key nHead y)) [] nTail
+      ) @ (resolve_match nTail)
+    in
+    resolve_match nestedHead
+  ) @ (resolve_matches nestedTail)
+
+
+(* make key set *)
+let rec replace_variable = fun (Var fromName, (toKey : rawKey)) (_rawKey : rawKey) : rawKey -> (
+  match _rawKey with
+  | Bar -> Bar
+  | Var _name -> if _name = fromName then toKey else _rawKey
+  | Node (_rk1, _rk2) -> Node ((replace_variable ((Var fromName), toKey) _rk1), (replace_variable ((Var fromName), toKey) _rk2))
+)
+
+let melt_match nameboxes match_rule =
+  List.map (fun (_expr, _rk) -> (_expr, replace_variable match_rule _rk)) nameboxes
+
+let melt_matches nameboxes match_rules =
+  List.fold_left (fun already x -> (melt_match nameboxes x)) nameboxes match_rules
+
+let into_bar (nameboxes : (map * rawKey) list) : key list = (
+  let rec barize _rawKey : key = 
+    match _rawKey with
+    | Bar | Var _ -> Bar
+    | Node (_rk1, _rk2) -> Node (barize _rk1, barize _rk2)
+  in
+  List.map (fun (_map, x) -> barize x) nameboxes
+)
+
+let getReady (_map : map) : key list = (
+  let resultEnv, usedVar = eval ([], _map, -1) in
+
+  (* Find nested variables *)
+  let nesteds = remove_map (remove_dupl (find_nested (find_sets resultEnv))) in
+
+  (* Find only namebox rules *)
+  let namebox_rules = only_namebox_rule resultEnv in
+
+  (* Find important matches *)
+  let resolved_matches = resolve_matches nesteds in
+
+  (* MELTDOWN! *)
+  let melted = melt_matches namebox_rules resolved_matches in
+
+  (* barize all *)
+  let result = remove_dupl (into_bar melted) in
+
+  result
+)
---
+++ HW4/test4_5.ml
@@ -0,0 +1,129 @@
+(* Exercise 2. getReady *)
+open Ex4_5
+open Testlib
+
+module TestEx3: TestEx =
+  struct
+    type testcase =
+      | GETREADY of map * key list
+      | FAIL of map
+
+    let testcases: testcase list =
+      [ GETREADY (End StarBox, [Bar])
+      ; GETREADY (End (NameBox "x"), [Bar])
+      ; GETREADY (Guide ("x", End (NameBox "x")), [Bar])
+      ; GETREADY (Branch (Guide ("x", End (NameBox "x")), End StarBox), [Bar])
+      ; FAIL (Branch (Guide ("x", Branch (End (NameBox "x"), End (NameBox "x"))), End StarBox))
+      ; GETREADY (Branch (Guide ("x", End (NameBox "x")), Guide ("y", End (NameBox "y"))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Branch (End (NameBox "x"), End StarBox), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Branch (Guide ("x", End (NameBox "x")), End StarBox), [Bar])
+      ; GETREADY (Guide ("x", Branch (End (NameBox "x"), End StarBox)), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Branch (Guide ("y", End (NameBox "y")), End (NameBox "x")), [Bar])
+      ; GETREADY (Branch (Guide ("y", End (NameBox "y")), Guide ("x", Guide ("x", End (NameBox "x")))), [Bar; Node (Bar, Node (Bar, Bar))])
+      ; GETREADY (Guide ("y", Guide ("x", Branch (End (NameBox "x"), End (NameBox "y")))), [Bar; Node (Bar, Bar)])
+      ; FAIL (Branch (End StarBox, End StarBox))
+      ; FAIL (Branch (End (NameBox "x"), End (NameBox "x")))
+      ; GETREADY (Branch (Guide ("x", End (NameBox "x")), End (NameBox "x")), [Bar])
+      ; GETREADY (Branch (Branch (End (NameBox "x"), End StarBox), Branch (End (NameBox "y"), End (NameBox "x"))), [Bar; Node (Bar, Node (Bar, Bar)); Node (Node (Bar, Node (Bar, Bar)), Bar)])
+      ; GETREADY (Branch (Branch (End (NameBox "x"), End (NameBox "y")), End (NameBox "y")), [Bar; Node (Bar, Node (Bar, Bar))])
+      ; FAIL (Guide ("x", Branch (End (StarBox), End (NameBox "x"))))
+      ; FAIL (Branch (Guide ("x", Branch (End (NameBox "x"), End (NameBox "x"))), End (StarBox)))
+      ; FAIL (Branch (Guide ("x", Branch (End (NameBox "x"), End (NameBox "x"))), Guide ("y", Branch (End (NameBox "y"), End (NameBox "y")))))
+      ; FAIL (Branch (Guide ("z", End (NameBox "z")), Branch (Guide ("x", Branch (End (NameBox "x"), End (NameBox "x"))), Guide ("y", Branch (End (NameBox "y"), End (NameBox "y"))))))
+      ; FAIL (Branch (Branch (End (NameBox "q"), End (NameBox "p")), Guide ("q", Branch (End (NameBox "p"), End (NameBox "q")))))
+      ; FAIL (Branch (Guide ("b", Branch (End (NameBox "a"), End (NameBox "b"))), Guide ("a", Branch (End (NameBox "b"), End (NameBox "a")))))
+      ; FAIL (Branch (Guide ("x", Branch (End (NameBox "y"), End (NameBox "x"))), Guide ("y", Branch (Branch (End (NameBox "z"), Branch (End (NameBox "y"), End (NameBox "w"))), End (NameBox "u")))))
+      ; FAIL (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "a"), End StarBox)))
+
+      ; FAIL (Branch (Guide ("x", (Guide ("y", Guide ("z", Guide ("w", Branch (Branch (End (NameBox "x"), End (NameBox "y")), Branch (End (NameBox "z"), End (NameBox "w")))))))), End StarBox))
+      ; FAIL (Guide ("x", Guide ("z", Guide ("y", Branch (Branch (Branch (End (NameBox "x"), End (NameBox "y")), Branch (End (NameBox "z"), End (NameBox "y"))), Branch (Branch (End (NameBox "x"), End (NameBox "y")), Branch (End (NameBox "z"), End (NameBox "y"))))))))
+      ; GETREADY (Guide ("a", Guide ("b", Branch (End (NameBox "b"), End (NameBox "a")))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Branch (Branch (End (NameBox "a"), End (StarBox)), End (NameBox "b")), [Bar; Node (Bar, Node (Bar, Bar))])
+      ; GETREADY (Guide ("x", Branch (End (NameBox "y"), Branch (End (NameBox "x"), End (StarBox)))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Branch (Guide ("y", End (NameBox "y")), Guide ("x", Branch (End (NameBox "x"), End StarBox))), [Bar;Node (Bar, Bar);Node (Node (Bar, Bar), Bar)])
+      ; GETREADY (Branch (End (NameBox "x"), Branch (End (NameBox "y"), Branch (End (NameBox "z"), End (StarBox)))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Guide ("x", Guide ("y", Branch (End (NameBox "x"), Branch (End (NameBox "y"), End (NameBox "x"))))), [Node (Bar, Bar); Node (Node (Bar, Bar), Bar)])
+      ; GETREADY (Guide ("x", Guide ("y", Branch (End (NameBox "y"), Branch (End (NameBox "x"), End StarBox)))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Guide ("a", Branch (Branch (End (NameBox "a"), End (StarBox)), Guide ("b", End (NameBox "b")))), [Bar; Node (Bar, Node (Node (Bar, Bar), Bar))])
+      ; GETREADY (Branch (Branch (End (NameBox "p"), Branch (End (NameBox "q"), End (StarBox))), Guide ("r", End (NameBox "r"))), [Bar; Node (Bar, Bar); Node (Bar, Node (Node (Bar, Bar), Bar))])
+      ; GETREADY (Branch (Branch (Guide ("x", End (NameBox "x")), Guide ("y", End (NameBox "y"))), Branch (End (NameBox "z"), End (StarBox))), [Bar; Node (Bar, Bar)])
+      ; GETREADY (Guide ("x", Guide ("y", Guide ("z", Branch (Branch (End (NameBox "x"), End (NameBox "y")), End (NameBox "z"))))), [Bar; Node (Bar, Node (Bar, Bar))])
+      ; GETREADY (Branch (Guide ("z", End (NameBox "z")), Guide ("x", Guide ("y", Branch (End (NameBox "x"), Branch (End (NameBox "y"), End (StarBox)))))), [Bar; Node (Bar, Bar); Node (Node (Bar, Bar), Node (Node (Bar, Bar), Bar))])
+      ; GETREADY (Branch (Branch (Branch (Guide ("t", Guide ("o", Branch (End (NameBox "o"), End (NameBox "t")))), Guide ("h", End (NameBox "h"))), Guide ("f", End (NameBox "f"))), End (NameBox "v")), [Bar; Node (Bar, Bar); Node (Node (Bar, Bar), Node (Bar, Bar))])
+      ; GETREADY (Branch (Branch (End (NameBox "x"), End (NameBox "y")), Guide ("y", Branch (End (NameBox "y"), End StarBox))), [Bar; Node (Bar, Bar); Node (Node (Bar, Bar), Node (Node (Node (Bar, Bar), Bar), Bar))])
+      ; GETREADY (Branch (Guide ("a", Guide ("b", Branch (End (NameBox "a"), End (NameBox "b")))), Guide ("c", Guide ("d", Branch (End (NameBox "d"), End (NameBox "c"))))), [Bar; Node (Bar, Bar); Node (Bar, Node (Node (Bar, Bar), Bar))])
+      ; GETREADY (Branch (Branch (Branch (Guide ("x", Guide ("y", Guide ("z", Branch (Branch (End (NameBox "x"), End (NameBox "y")), End (NameBox "z"))))), End (NameBox "a")), End (NameBox "b")), End (NameBox "c")), [Bar; Node (Bar, Node (Bar, Bar))])
+      ; GETREADY (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "b"), End (StarBox))), [Bar; Node (Bar, Bar); Node (Bar, Node (Bar, Bar)); Node (Node (Bar, Bar), Node (Bar, Node (Bar, Node (Bar, Bar))))])
+      ; GETREADY (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "c"), End StarBox)), [Bar; Node (Bar, Bar); Node (Bar, Node (Bar, Bar)); Node (Bar, Node (Node (Bar, Bar), Node (Bar, Node (Bar, Bar))))])
+      ; GETREADY (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "d"), End StarBox)), [Bar; Node (Bar, Node (Bar, Bar)); Node (Bar, Node (Bar, Node (Bar, Node (Node (Bar, Bar), Bar))))])
+      ; GETREADY (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "e"), End StarBox)), [Bar; Node (Bar, Bar); Node (Node (Bar, Bar), Node (Bar, Bar)); Node (Bar, Node (Bar, Node (Bar, Node (Bar, Bar))))])
+      ; GETREADY (Branch (Branch (Branch (Branch (End (NameBox "a"), End (NameBox "b")), End (NameBox "c")), Branch (Branch (End (NameBox "d"), End (NameBox "e")), End (NameBox "f"))), Branch (End (NameBox "f"), End StarBox)), [Bar; Node (Bar, Bar); Node (Bar, Node (Node (Bar, Bar), Bar)); Node (Bar, Node (Bar, Node (Bar, Node (Bar, Bar))))])
+      ; GETREADY (Branch (Guide ("x", (Guide ("y", Guide ("z", Guide ("w", Branch (Branch (End (NameBox "x"), End (NameBox "y")), Branch (End (NameBox "z"), End (NameBox "w")))))))), Guide ("a", Branch (End (NameBox "a"), End (NameBox "b")))), [Bar; Node (Bar, Bar); Node (Bar, Node (Bar, Bar)); Node (Node (Bar, Node (Bar, Bar)), Node (Bar, Bar))])
+      ; FAIL (Branch (Guide ("c", Branch (Branch (Guide ("a", Branch (End (NameBox "a"), End (NameBox "b"))), Branch (End (NameBox "b"), End (NameBox "a"))), End (NameBox "c"))), End StarBox))
+      ; GETREADY (Branch (End (NameBox "z"), Guide ("x", Branch (Guide ("y", Branch (End (NameBox "x"), End (NameBox "y"))), End StarBox))), [Bar; (Node (Bar, Bar)); (Node (Node (Node (Bar, Bar), Bar), Bar))])
+      ; GETREADY (Branch (Branch (Branch (Guide ("b", Guide ("a", Branch (End (NameBox "a"), End (NameBox "b")))), End (NameBox "c")), Guide ("d", End (NameBox "d"))), End (NameBox "e")), [Bar; Node (Bar, Bar); Node (Node (Bar, Bar), Node (Bar, Bar))])
+      ]
+
+    let string_of_treasure t =
+      match t with
+      | StarBox -> "*"
+      | NameBox name -> name
+
+    let rec string_of_map m =
+      match m with
+      | End t -> string_of_treasure t
+      | Branch (m1, m2) -> Printf.sprintf "(%s | %s)" (string_of_map m1) (string_of_map m2)
+      | Guide (id, m) -> Printf.sprintf "([%s]%s)" id (string_of_map m)
+
+    let rec string_of_key (k : key) =
+      match k with
+      | Bar -> "-"
+      | Node (k1, k2) -> Printf.sprintf "(%s, %s)" (string_of_key k1) (string_of_key k2)
+
+    let string_of_key_list =
+      string_of_list string_of_key
+
+    let sort = List.sort compare
+
+    let runner (tc: testcase): bool =
+      match tc with
+      | GETREADY (m, ans) ->
+          let output =
+            try Some (getReady m |> sort)
+            with IMPOSSIBLE -> None
+          in output = Some (ans |> sort)
+      | FAIL m ->
+          let output =
+            try Some (getReady m)
+            with IMPOSSIBLE -> None
+          in output = None
+
+    let string_of_output out =
+      match out with
+      | Some res -> string_of_key_list res
+      | None -> "exception IMPOSSIBLE"
+
+    let string_of_tc (tc: testcase): string * string * string =
+      match tc with
+      | GETREADY (m, ans) ->
+          let output =
+            try Some (getReady m |> sort)
+            with IMPOSSIBLE -> None
+          in
+          ( Printf.sprintf "\n  getReady  %s" (string_of_map m)
+          , string_of_output (Some (ans |> sort))
+          , string_of_output output
+          )
+      | FAIL m ->
+          let output =
+            try Some (getReady m |> sort)
+            with IMPOSSIBLE -> None
+          in
+          ( Printf.sprintf "\n  getReady  %s" (string_of_map m)
+          , string_of_output None
+          , string_of_output output
+          )
+  end
+
+open TestEx3
+let _ = wrapper testcases runner string_of_tc
