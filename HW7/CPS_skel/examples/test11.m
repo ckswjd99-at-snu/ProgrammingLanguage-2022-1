b663d65c88eafa152af00406668cc843c2a75528
chanjeong park <ckswjd99@snu.ac.kr>
2022-06-03 11:15:40 +0900

    submit

---
+++ HW7/CPS_skel/examples/test11.m
@@ -0,0 +1,3 @@
+(* testcase 11 : ifp *)
+
+ifp ((fn x => x + 1) (-1) + 1) then ((fn y => y + 3) 0) else 0
--- HW7/CPS_skel/test.sh
+++ HW7/CPS_skel/test.sh
@@ -4,6 +4,7 @@ ls ./examples/*.m > ./testfiles.temp
 echo "" > ./testresult.temp
 while read line || [ -n "$line" ]
 do
+  echo $line
   ./run $line
 done < testfiles.temp
 
